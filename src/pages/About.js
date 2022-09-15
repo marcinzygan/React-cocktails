e6905c9185661b74776dc32785d62b02ef3a6588
marcinzygan <soul@o2.pl>
2022-09-15 17:22:13 +0200

    added about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+        officiis repellendus eius optio temporibus. Fugit consequatur rerum
+        eligendi tempore dolore, veritatis optio dignissimos. Illum, ipsam!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
