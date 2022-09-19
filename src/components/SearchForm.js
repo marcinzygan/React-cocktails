ba8633db1f45aff849b5f3ab69bb25cfa1219681
marcinzygan <soul@o2.pl>
2022-09-19 17:10:53 +0200

    added prevent default to search form , added focus for search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,12 +4,21 @@ import { useGlobalContext } from "../context";
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   const searchCocktail = function () {
     setSearchTerm(searchValue.current.value);
   };
+  const handleSubmit = function (e) {
+    e.preventDefault();
+  };
+
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
